756da58d93e5adceb5e124ff63ef18fc4db45b8d
thelaceyedit <thelaceyedit@gmail.com>
2018-09-14 16:05:13 -0400

    sql

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+insert into burgers (burger_name, devoured)
+value ("Jr Bacon Cheeseburger", false);
+
+insert into burgers (burger_name, devoured)
+value ("Bean Burger", false);
+
+insert into burgers (burger_name, devoured)
+value ("Little Cheeseburger with Lettuce Bun", false)
+
+insert into burgers (burger_name, devoured)
+value ("Greensburger", false);
+
+insert into burgers (burger_name, devoured)
+value ("Big Mac", false);
+
