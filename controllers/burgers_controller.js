7f03ca822e87d342f22f9ab4fc449e41084085f5
thelaceyedit <thelaceyedit@gmail.com>
2018-09-14 17:43:17 -0400

    models

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,16 +1,44 @@
 var express = require ("express");
 
-var burger = require ("./models/burger.js");
-
 var router = express.Router();
 
+var burger = require ("./models/burger.js");
 
 
-router.get();
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var burgerObject = {
+            burger: data
+        };
+        console.log(burgerObject);
+        res.render("index", burgerObject);
+    });
+});
 
-router.post();
+router.post("/models/burger.js", function(req,res) {
+    burger.create(["name", "devoured"], [req.body.name, req.body.devoured], function(result) {
+        res.json({ id: result.insertId});
+    });
+});
 
-router.put();
+router.put("/models/burger.js/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burger.update(
+        {
+            devoured: req.body.devoured
+        },
+        condition,
+        function(result) {
+            if(result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
 
 
 module.exports = burgers_controller.js;
